f594e341f090587e1903d9c6bd9d142e8c292fb1
fe3dback <fe3dback@yandex.ru>
2018-02-25 03:32:06 +0300

    add converalls conf, initial struct fix

--- benchmarks/FS/StrBench.php
+++ benchmarks/FS/StrBench.php
@@ -2,25 +2,25 @@
 
 declare(strict_types=1);
 
-class SModifyBench
+class StrBench
 {
     public function benchFS_EnsureLeft(): void
     {
-        \FS\Lib\SCommon::ensureLeft('2', '1');
+        (new \FS\Str('2'))->ensureLeft('1');
     }
 
     public function benchStringy_EnsureLeft(): void
     {
-        (\Stringy\create('2'))->ensureLeft('1');
+        (new Stringy\Stringy('2'))->ensureLeft('1');
     }
 
     public function benchFS_EnsureRight(): void
     {
-        \FS\Lib\SCommon::ensureLeft('1', '2');
+        (new \FS\Str('1'))->ensureRight('2');
     }
 
     public function benchStringy_EnsureRight(): void
     {
-        (\Stringy\create('1'))->ensureRight('2');
+        (new Stringy\Stringy('1'))->ensureRight('1');
     }
 }
